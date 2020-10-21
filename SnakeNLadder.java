40da26139d9c309dc96da80919f4ff9b1d070dcf
Nisha271 <bukkenisha@gmail.com>
2020-10-22 00:16:49 +0530

    [Archana] Added: UC2 with Player rolls the die

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,7 +1,15 @@
+import java.util.Random;
 public class SnakeNLadder{
 	public static int position = 0;
 	int player1 = 0;
+	 public static int rolldice(int number) {
+                int n = 0;
+                Random r = new Random();
+                n=r.nextInt(number);
+                return (n==0?1:n);
+        }
 	public static void main(String[] args){
-		System.out.println("Player1 position: " +position);
+		 int rolldice = rolldice(7);
+		System.out.println("Roll Dice Value: " + rolldice);
 	}
 }
