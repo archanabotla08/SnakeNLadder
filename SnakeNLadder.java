90d5b38f8b0e74eea285584b7f3111d4b6e4ba84
Nisha271 <bukkenisha@gmail.com>
2020-10-22 00:06:42 +0530

    [Archana] Added: UC1 with SinglePlayer and position initialized

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,5 +1,7 @@
 public class SnakeNLadder{
-		public static void main(String[] args){
-			System.out.println("Welcome to Master");
-		}
+	public static int position = 0;
+	int player1 = 0;
+	public static void main(String[] args){
+		System.out.println("Player1 position: " +position);
 	}
+}
