d68831afe59f86ee78aa5945000c3ee95ea4c97b
Nisha271 <bukkenisha@gmail.com>
2020-10-22 00:26:36 +0530

    [Archana] Added: UC3 with Player checks for NoPlay,Ladder or Snake

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,15 +1,50 @@
 import java.util.Random;
 public class SnakeNLadder{
+	public static final int NOPLAY = 1;
+        public static final int LADDER = 2;
+        public static final int SNAKE = 3;
+
 	public static int position = 0;
+
 	int player1 = 0;
-	 public static int rolldice(int number) {
+
+	public static int rolldice(int number) {
                 int n = 0;
                 Random r = new Random();
                 n=r.nextInt(number);
                 return (n==0?1:n);
         }
+	public static void startGame() {
+                        int random = rolldice(4);
+                        int rolldice = rolldice(7);
+                        System.out.println("---------------------");
+                        System.out.println("Roll Dice Value : " + rolldice);
+                        System.out.println("Random value : " + random);
+                        switch (random) {
+                        case NOPLAY:
+                                System.out.println("NOPLAY");
+                                System.out.println("Position of player : " + position);
+                                System.out.println("----------------------");
+                                break;
+                        case LADDER:
+                                System.out.println("############## Climb the LADDER ############");
+                                position+=rolldice;
+                                System.out.println("Position of the  Player = " + position);
+                                System.out.println("----------------------");
+                                break;
+                        case SNAKE:
+                                System.out.println("~~~~~~~~~~~~~~~~ Bites the SNAKE ~~~~~~~~~~~~");
+                                position-=rolldice;
+                                System.out.println("Position of the  Player = " + position);
+                                System.out.println("----------------------");
+                                break;
+                        default:
+                                break;
+                        }
+        }
+
 	public static void main(String[] args){
-		 int rolldice = rolldice(7);
-		System.out.println("Roll Dice Value: " + rolldice);
+		System.out.println("Player Position At Start: "+ position );
+                startGame();
 	}
 }
