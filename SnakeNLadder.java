145ec99aa4a94b640aaf1bcfc4265899236739ff
Nisha271 <bukkenisha@gmail.com>
2020-10-22 08:35:58 +0530

    [Archana] Added: UC7 with two players playing the game and if Ladder occurs than roll the dice twice

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -7,12 +7,13 @@ public class SnakeNLadder{
         public static int WININGTHROWS = 0;
         public static int LOSINGTHROWS = 0;
         public static int NOPLAYTHROWS = 0;
-
+	public static int currentPlayer = -1;
 	final static int WINPOINT = 100;
 
 	public static int position = 0;
 	public static int calculatedPosition = 0;
-	int player1 = 0;
+	public static int player1 = 0;
+	public static int player2 = 0;
 
 	public static int rolldice(int number) {
                 int n = 0;
@@ -21,7 +22,7 @@ public class SnakeNLadder{
         	COUNTER++;
 	        return (n==0?1:n);
         }
-	public static void startGame() {
+	public static int startGame() {
                     do{
 		    	int random = rolldice(4);
                         int rolldice = rolldice(7);
@@ -40,6 +41,9 @@ public class SnakeNLadder{
                                 System.out.println("############## Climb the LADDER ############");
                                 calculatedPosition = calculatePlayerLadderPosition(calculatedPosition,rolldice);
                                 System.out.println("Position of the  Player = " + calculatedPosition);
+				int rolldice1 = rolldice(7);
+				calculatedPosition = calculatePlayerLadderPosition(calculatedPosition,rolldice1);
+				System.out.println("Position of the  Player = " + calculatedPosition);
                                 System.out.println("----------------------");
                                 break;
                         case SNAKE:
@@ -52,8 +56,42 @@ public class SnakeNLadder{
                         default:
                                 break;
                         }
+			return calculatedPosition;
                    }while(calculatedPosition != WINPOINT);
         }
+		public static void twoPlayerGame() {
+		do {
+		System.out.print(currentPlayer == -1 ? "\n\n **** First Player Turn ****" : "\n\n **** Second Player Turn ****");
+		
+		if(currentPlayer == -1) {
+			player1 = startGame();
+			System.out.println("First Player Values " + player1);
+			System.out.println("Second Player Values " + player2);
+			System.out.println("------------------------------");
+			if(isWin(player1)) {
+				System.out.println("First Player Wins  " + player1);
+				System.out.println("------------------------------");
+				return;
+			}
+			
+		}else {
+			player2 = startGame();
+			System.out.println("\nFirst Player Values " + player1);
+			System.out.println("\nSecond Player Values " + player2);
+			System.out.println("------------------------------");
+			if(isWin(player2)) {
+				System.out.println("\nSecond Player Wins  " + player2);
+				System.out.println("------------------------------");
+				return;
+			}
+			
+		}
+		currentPlayer = -currentPlayer;
+		}while(player1 != 100 || player2 != 100);
+	}
+	public static boolean isWin(int player) {
+		return WINPOINT == player;
+	}
 
 	 public static int calculatePlayerLadderPosition(int position,int dicevalue) {
                 position = position + dicevalue;
@@ -77,7 +115,7 @@ public class SnakeNLadder{
 		System.out.println("---------------------- START-----------------------" );
 
 		System.out.println("Player Position At Start: "+ position );
-                startGame();
+                twoPlayerGame();
 		System.out.println("---------------------- END -----------------------" );
 
 		System.out.println("NOPLAY Rolls of Die: " + NOPLAYTHROWS);
